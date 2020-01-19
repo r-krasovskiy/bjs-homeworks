e2f03079e3b90cb96bb22138ad4df6ccfc75a6ea
roman-krasowski <54819181+roman-krasowski@users.noreply.github.com>
2020-01-19 22:33:08 +0300

    initial commit

---
+++ main.js
@@ -0,0 +1,16 @@
+//Assignmentt 1.1
+let algebra = 5, geography = 4, physics = 4,
+    avgGrade = (algebra + geography + physics) / 3;
+console.log(avgGrade);
+console.log('\n');
+
+//Assignmentt 1.2
+let myName = 'Роман',
+    message = `Привет, мир! Меня зовут ${myName}.`
+console.log(message);
+console.log('\n');
+
+//Assignmentt 1.3
+let x =2, y = 22, z = 0,
+    result = x * y + 5 * z + x - 1;
+console.log(result);
